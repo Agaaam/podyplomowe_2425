7b3530f9f77ee0fb376287c1972f8a1df262e389
Agnieszka <marczyk-agnieszka@wp.pl>
2024-11-17 13:38:48 +0100

    cwiczenie z csv-histogram

--- Zajecia_20241117.py
+++ Zajecia_20241117.py
@@ -1,5 +1,21 @@
 #zaladowanie biblioteki pandas
-import panda
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 #Zasilenie csv
+df = pd.read_csv('Pliki_do_cwiczen\\weight-height.csv', delimiter=';')
+print(df)
+(print(type(df)))
 
+print(df.describe())
+
+df.Height *= 2.54
+df.Weight /= 2.2
+print(df.head(3))
+
+
+print('\nWykres')
+plt.hist(df.Weight)
+plt.show()

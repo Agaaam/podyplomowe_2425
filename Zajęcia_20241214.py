9c86b4b76568176254f0d0e740c6d9e40a53ee40
Agnieszka <marczyk-agnieszka@wp.pl>
2024-12-14 09:25:32 +0100

    aktu2

--- Zajęcia_20241214.py
+++ Zajęcia_20241214.py
@@ -3,6 +3,23 @@
 # git commit -m 'wiadomosc' - opis comita
 # git push - puszczenie aktualizacji na github
 
+#powtórka  z printów i inputów
+# print('Siema')
+# input()
+# age = int(input('Ile masz lat?  '))
+# print(f'Wiec masz {age} lat')
+# print(f'Będziesz dorosły za {18 - age} lat')
 
-print('Siema')
-age = input('Ile masz lat?  ')
+
+#Powtórka z while true (czyli rób tak długo dopóki nie wystąpi) oraz z if'a
+while True:
+    age = int(input('Ile masz lat? '))
+    if 0 < age <18:
+        print(f'Wiec masz {age} lat')
+        print(f'Będziesz dorosły za {18 - age} lat')
+        break
+
+    else:
+        print('zły wiek, jeszcze raz')
+
+print('dalsza częsc programu')

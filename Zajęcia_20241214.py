894a4a9c58eb68df88f15994cca077e62558f6e8
Agnieszka <marczyk-agnieszka@wp.pl>
2024-12-14 09:08:42 +0100

    2

--- Zajęcia_20241214.py
+++ Zajęcia_20241214.py
@@ -1,4 +1,8 @@
 #przypomnienie git hub, komendy
+# git add . -  czyli dodanie pliku do obserwacji
+# git commit -m 'wiadomosc' - opis comita
+# git push - puszczenie aktualizacji na github
+
 
 print('Siema')
 age = input('Ile masz lat?  ')

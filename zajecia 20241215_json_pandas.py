315dbbd5d8c5e4cabccc12e41dea59c8a22c52b4
Agnieszka <marczyk-agnieszka@wp.pl>
2024-12-15 10:30:04 +0100

    json na df

--- zajecia 20241215_json_pandas.py
+++ zajecia 20241215_json_pandas.py
@@ -1,5 +1,5 @@
 import requests
-import pandas
+import pandas as pd
 import matplotlib.pyplot as plt
 
 #1. pobieranie danych z publicznego api
@@ -11,5 +11,24 @@ response_users = requests.get(url_users)
 response_posts = requests.get(url_posts)
 response_comments = requests.get(url_comments)
 
-print(response_users)
-print(response_users.json())
+# print(response_users)
+# print(response_users.json())
+# print(response_users.status_code)
+
+if response_users.status_code == 200 and response_posts.status_code == 200 and response_comments.status_code == 200 :
+    data_users = response_users.json()
+    data_posts = response_posts.json()
+    data_comments = response_comments.json()
+else:
+    raise EXCEPTION("nie udało się pobrać danych")
+
+
+print(data_users)
+print(data_posts)
+print(data_comments)
+
+#tworzenie data frames z jsonów
+df_users = pd.DataFrame(data_users)
+df_posts = pd.DataFrame(data_posts)
+df_comments = pd.DataFrame(data_comments)
+

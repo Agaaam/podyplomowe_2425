50f683a0ddbde6c5a74d7a98160352884e389f28
Agnieszka <marczyk-agnieszka@wp.pl>
2025-01-12 14:39:54 +0100

    ostatni

--- ZAJECIA_KAFKA/Zajecia_20250112_6_KAFKA_consumer.py
+++ ZAJECIA_KAFKA/Zajecia_20250112_6_KAFKA_consumer.py
@@ -15,28 +15,37 @@ class WeatherDataConsumer:
         print(f"Konsument zainicjowany...")
 
     def start_consuming(self):
-        while True:
-            msg = self.consumer.poll(1.0)#sprawdzamy co sekunde czy są nowe wiaodmości
 
-            if msg is None: #jeśli jest puste to kontynuuj, ciągle sprawdza czy coś się pojawiło
-                continue
-            if msg.error():
-                if msg.error().code() == KafkaError._PARTITION_EOF:
+        with open('weather_data.csv','a') as f:
+            if f.tell()==0:
+                f.write("timestamp, station_id, temperature\n")
+
+            while True:
+                msg = self.consumer.poll(1.0)#sprawdzamy co sekunde czy są nowe wiaodmości
+
+                if msg is None: #jeśli jest puste to kontynuuj, ciągle sprawdza czy coś się pojawiło
                     continue
-                else:
-                    print(f"Bląd: {msg.error()}")
-                    break
-            try:
-                data = json.loads(msg.value().decode("utf-8"))
-                station_id = data['station_id']
-                print(f"Otrzymano zadanie dla stacji: {station_id}")
+                if msg.error():
+                    if msg.error().code() == KafkaError._PARTITION_EOF:
+                        continue
+                    else:
+                        print(f"Bląd: {msg.error()}")
+                        break
+                try:
+                    data = json.loads(msg.value().decode("utf-8"))
+                    station_id = data['station_id']
+                    print(f"Otrzymano zadanie dla stacji: {station_id}")
 
-                weather_data = fetch_weather_data(station_id, False)
-                if weather_data:
-                    print(f"Pobrano dane {weather_data}")
+                    weather_data = fetch_weather_data(station_id, False)
+                    if weather_data:
+                        timestamp = weather_data['timestamp']
+                        temp = weather_data['temperature']
+                        f.write(f"{timestamp},{station_id},{temp}\n")
+                        f.flush()
+                        print(f"Pobrano dane {weather_data}")
 
-            except Exception as e:
-                print(f"Błąd przetwarzania wiadomości: {e}")
+                except Exception as e:
+                    print(f"Błąd przetwarzania wiadomości: {e}")
 
 
 if __name__ == "__main__":

9580ce4dbac9d1288e5dfb929c120f3d9e043e17
Agnieszka <marczyk-agnieszka@wp.pl>
2025-01-12 09:27:50 +0100

    1

---
+++ Zajecia_20250112_1_RED.py
@@ -0,0 +1,16 @@
+"""
+#Notatka z pracy na CMD, REDIS
+wsl --install
+sudo apt-get update
+sudo apt-get install redis-server
+sudo service redis - server start
+sudo service redis - server status
+sudo service redis-server stop
+redis-cli
+info - pokazują się statystyki
+set -
+keys * - wyświetla wszystkie klucze
+dbsize -
+flushall - kasuje wszystkie klucze
+"""
+

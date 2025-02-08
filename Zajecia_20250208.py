5c50080496797b7e9e0f80a437a07fae18c9ead8
Agnieszka <marczyk-agnieszka@wp.pl>
2025-02-08 09:57:26 +0100

    3

--- Zajecia_20250208.py
+++ Zajecia_20250208.py
@@ -1,8 +1,25 @@
 #regresja liniowa
-
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv('Pliki_do_cwiczen\\AI\\weight-height.csv')
-print(df.head(10))
+print(df.head(5))
 
 print(df.Gender.value_counts())
+
+df.Height *= 2.54 #przemnażamy całą kolumnę aby były cm
+print(f'Po zmianie jednostek: \n{df.head(5)}')
+
+df.Weight /= 2.2 #przemnażamy całą kolumnę aby były kg
+print(f'Po zmianie jednostek: \n{df.head(5)}')
+
+#niezależne 2 kolumny - gender i height. Wynik weight
+#wszystkie płcie
+plt.hist(df.Weight, bins=50)
+plt.show()
+#osobno panie i panowie na jednym wykresie. Aby były dwa osobne to musimy uruchomish zahaszowany kod
+plt.hist(df.query("Gender=='Male'").Weight, bins=50)
+#plt.show() #
+plt.hist(df.query("Gender=='Female'").Weight, bins=50)
+plt.show()

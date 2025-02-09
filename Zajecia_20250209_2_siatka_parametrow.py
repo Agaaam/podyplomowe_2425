b75604a22c2ffc5ff6c810398ffcec4840a83689
Agnieszka <marczyk-agnieszka@wp.pl>
2025-02-09 10:25:09 +0100

    aktu

--- Zajecia_20250209_2_siatka_parametrow.py
+++ Zajecia_20250209_2_siatka_parametrow.py
@@ -8,17 +8,14 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 
-df = pd.read_csv('Pliki_do_cwiczen\\heart.csv', comment='#')
+df = pd.read_csv('dane\\heart.csv', comment='#')
 print(df.head().to_string())
 
-
-print('Klasyfikator Decission Tree')
 X = df.iloc[:, :-1]
 y = df.target
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-model = DecisionTreeClassifier(criterion='gini', max_depth=5, min_samples_split=2)
+model = DecisionTreeClassifier(criterion='gini', max_depth=None, min_samples_split=5)
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
 print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
@@ -27,8 +24,12 @@ print(pd.DataFrame(model.feature_importances_, X.columns))
 print('Siatka parametrów')
 model = DecisionTreeClassifier()
 params = {
-    'max_depth': range(5,20), #[2, 3, 4],
-    'criterion': ['gini', 'entropy', 'log_loss']
+    'max_depth': range(5, 20),
+    'criterion': ['gini', 'entropy', 'log_loss'],
+    'min_samples_split': range(2, 5),
+    'max_features': range(3, X_train.shape[1]+1, 2),
+    'min_samples_leaf': range(1, 5),
+    'min_weight_fraction_leaf': np.arange(0, 3, 0.5)
 }
 
 grid = GridSearchCV(model, params, cv=5, verbose=2)

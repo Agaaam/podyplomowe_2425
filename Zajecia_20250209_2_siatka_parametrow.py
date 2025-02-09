0cadbe8a09512f70ef2e8985a8edaff86572e966
Agnieszka <marczyk-agnieszka@wp.pl>
2025-02-09 10:15:52 +0100

    losowy

--- Zajecia_20250209_2_siatka_parametrow.py
+++ Zajecia_20250209_2_siatka_parametrow.py
@@ -8,5 +8,30 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 
-df = pd.read_csv('Pliki_do_cwiczen\\heart.csv')
-print(df['class'].value_counts())
+df = pd.read_csv('Pliki_do_cwiczen\\heart.csv', comment='#')
+print(df.head().to_string())
+
+
+print('Klasyfikator Decission Tree')
+X = df.iloc[:, :-1]
+y = df.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = DecisionTreeClassifier(criterion='gini', max_depth=5, min_samples_split=2)
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+print(pd.DataFrame(model.feature_importances_, X.columns))
+
+print('Siatka parametrów')
+model = DecisionTreeClassifier()
+params = {
+    'max_depth': range(5,20), #[2, 3, 4],
+    'criterion': ['gini', 'entropy', 'log_loss']
+}
+
+grid = GridSearchCV(model, params, cv=5, verbose=2)
+grid.fit(X_train, y_train)
+print(f'parametry: {grid.best_params_}')
+print(f'Wynik: {grid.best_score_}')

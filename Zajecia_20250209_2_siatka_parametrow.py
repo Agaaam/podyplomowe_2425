da3c81cf3837ce93e92405f33bf81b07efa13f15
Agnieszka <marczyk-agnieszka@wp.pl>
2025-02-09 09:31:53 +0100

    nowy plik csv

--- Zajecia_20250209_2_siatka_parametrow.py
+++ Zajecia_20250209_2_siatka_parametrow.py
@@ -7,3 +7,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
+
+df = pd.read_csv('Pliki_do_cwiczen\\heart.csv')
+print(df['class'].value_counts())
